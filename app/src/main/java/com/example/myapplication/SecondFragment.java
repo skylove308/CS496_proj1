16245e438332bcbef3b1801eeda4661bda5cc5cd
HyeWon <un0211@kaist.ac.kr>
2017-07-03 01:38:42 +0900

    B탭 기초 틀 완성. GridView로 이미지 보여주기.

--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -1,15 +1,23 @@
 package com.example.myapplication;
 
 
+import android.content.Context;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 
 /**
@@ -17,23 +25,87 @@ import android.widget.ListView;
  */
 public class SecondFragment extends Fragment {
 
-
-    static final String[] LIST_CONTACT = {"PERSON1", "PERSON2", "PERSON3", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P", "P"};
-
     public SecondFragment() {
         // Required empty public constructor
     }
 
+    public class SquareImageView  extends ImageView {
+
+        public SquareImageView(Context context) {
+            super(context);
+        }
+
+        public SquareImageView(Context context, AttributeSet attrs) {
+            super(context, attrs);
+        }
+
+        public SquareImageView(Context context, AttributeSet attrs, int defStyle) {
+            super(context, attrs, defStyle);
+        }
+
+        @Override
+        protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+            int width = getMeasuredWidth();
+            setMeasuredDimension(width, width);
+        }
+
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_second, null);
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(), android.R.layout.simple_list_item_1, LIST_CONTACT);
+        class ImageAdapter extends BaseAdapter {
+            private Context context;
 
-        ListView listview = (ListView) view.findViewById(R.id.photo_list);
-        listview.setAdapter(adapter);
+            private Integer[] images = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
+                    R.drawable.img_5, R.drawable.img_6, R.drawable.img_7, R.drawable.img_8, R.drawable.img_9,
+                    R.drawable.img_10, R.drawable.img_11, R.drawable.img_12, R.drawable.img_13, R.drawable.img_14,
+                    R.drawable.img_15, R.drawable.img_16, R.drawable.img_17,
+            };
+
+            public ImageAdapter(Context c) {
+                this.context = c;
+            }
+
+            public int getCount() {
+                return images.length;
+            }
+
+            public Object getItem(int position) {
+                return position;
+            }
+
+            public long getItemId(int position) {
+                return position;
+            }
+
+            public View getView(int position, View convertView, ViewGroup parent) {
+                SquareImageView imageView;
+
+                if (convertView == null) {
+                    imageView = new SquareImageView(context);
+                } else {
+                    imageView = (SquareImageView) convertView;
+                }
+                imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                
+                imageView.setImageResource(images[position]);
+                return imageView;
+            }
+        }
+        View view = inflater.inflate(R.layout.fragment_second, null);
+
+        GridView gridview = (GridView) view.findViewById(R.id.photo_list);
+        gridview.setAdapter(new ImageAdapter(getActivity()));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+            }
+        });
 
         FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab2);
         fab.setOnClickListener(new View.OnClickListener() {
