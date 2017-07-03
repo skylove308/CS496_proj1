547d15beafebe3c3332acc762a681bc254cc066f
skylove308 <skylove0911@naver.com>
2017-07-03 20:40:42 +0900

    A tap 전화걸기, 사진 추가

--- app/src/main/java/com/example/myapplication/ContactDetail.java
+++ app/src/main/java/com/example/myapplication/ContactDetail.java
@@ -1,13 +1,20 @@
 package com.example.myapplication;
 
+import android.app.ActionBar;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+
 
 /**
  * Created by q on 2017-07-02.
  */
+
 
 public class ContactDetail extends AppCompatActivity {
     @Override
@@ -17,10 +24,29 @@ public class ContactDetail extends AppCompatActivity {
 
         TextView name = (TextView)findViewById(R.id.textView1);
         TextView number = (TextView)findViewById(R.id.textView2);
+        ImageView picture = (ImageView)findViewById(R.id.imageView);
+        ImageView phone = (ImageView)findViewById(R.id.button1);
+        ImageView modify = (ImageView)findViewById(R.id.button2);
+
 
         Intent intent = getIntent();
         name.setText(intent.getStringExtra("name"));
         number.setText(intent.getStringExtra("number"));
+        picture.setImageResource(intent.getIntExtra("picture",0));
 
+
+
+    phone.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View v){
+        Intent intent = getIntent();
+        String phonenumber = intent.getStringExtra("number");
+        String tel = "tel:" + phonenumber;
+        Intent myintent = new Intent(Intent.ACTION_VIEW, Uri.parse(tel));
+        startActivity(myintent);
     }
+    });
+}
 }
--- app/src/main/java/com/example/myapplication/SecondFragment.java
+++ app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -32,7 +32,7 @@ public class SecondFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public class SquareImageView  extends ImageView {
+    public class SquareImageView  extends android.support.v7.widget.AppCompatImageView {
 
         public SquareImageView(Context context) {
             super(context);
@@ -79,7 +79,7 @@ public class SecondFragment extends Fragment {
         class ImageAdapter extends BaseAdapter {
             private Context context;
 
-            private Integer[] images = {R.drawable.img_1, R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
+            private Integer[] images = {R.drawable.img_1,R.drawable.img_2, R.drawable.img_3, R.drawable.img_4,
                     R.drawable.img_5, R.drawable.img_6, R.drawable.img_7, R.drawable.img_8, R.drawable.img_9,
                     R.drawable.img_10, R.drawable.img_11, R.drawable.img_12, R.drawable.img_13, R.drawable.img_14,
                     R.drawable.img_15, R.drawable.img_16, R.drawable.img_17,
